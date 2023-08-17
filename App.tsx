dd22346422e8ed561855cd3632d60bba9aa91ac6
eduardoarad <araujocarlos893@gmail.com>
2023-08-17 09:34:30 -0300

    Login Social

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -6,7 +7,6 @@ import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 
 import theme from './src/theme';
-import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
