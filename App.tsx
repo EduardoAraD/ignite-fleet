a8d046db374ca46ff8ceef1bb31234c75cab70fc
eduardoarad <araujocarlos893@gmail.com>
2023-08-17 11:25:00 -0300

    Cloud MongoDB Atlas

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { AppProvider, UserProvider } from '@realm/react';
+
+import { REALM_APP_ID } from '@env'
 
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 
 import { Loading } from './src/components/Loading';
 
@@ -16,13 +20,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
+import { Realm, useApp } from '@realm/react';
 
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
 
@@ -13,6 +14,8 @@ import backgroundImg from '../../assets/background.png';
 WebBrowser.maybeCompleteAuthSession();
 
 export function SignIn() {
+  const app = useApp();
+
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   const [_, response, googleSignIn] = Google.useAuthRequest({
@@ -34,9 +37,18 @@ export function SignIn() {
   useEffect(() => {
     if(response?.type === 'success'){
       if(response.authentication?.idToken){
+        // pegar os dados do usuario do token =>
         // fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication?.idToken}`)
         //   .then(response => response.json())
         //   .then(console.log);
+
+
+        const credentials = Realm.Credentials.jwt(response.authentication.idToken);
+        app.logIn(credentials).catch((error) => {
+          console.log(error);
+          Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta google.')
+          setIsAuthenticating(false);
+        });
       } else {
         Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta google.')
         setIsAuthenticating(false);
